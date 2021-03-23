191f850f330aac76c4f918553a208772c50f218a
Ahmed_Home <Ahmed_Home@DESKTOP-LMHQ5LT>
2021-03-23 17:17:47 -0400

    first commit Homework

---
+++ Homework/src/Variables/instanceVariable.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class instanceVariable {
+
+	 String name = "charlie";
+     int age = 5;
+     
+     public void name() {
+ 		name="charlie";
+     }
+     
+     public void age() {
+ 		age= 5;
+     }
+}
---
+++ Homework/src/Variables/localVariable1.java
@@ -0,0 +1,14 @@
+package Variables;
+
+public class localVariable1 {
+	
+	public void age() {
+		int age= 34;
+	} 
+	
+	public void name() {
+		String name ="rubina"; 
+		
+	}
+	
+}
